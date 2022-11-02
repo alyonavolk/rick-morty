2e5bce5fcfbac189eba3ff3693afdee4740099c8
Alyona Volkova <alena18252830@gmail.com>
2022-11-02 23:30:36 +0500

    Adjusting style.css, Header and Card folders

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,23 +5,32 @@ import Banner from '../Banner/Banner';
 import Search from '../Search/Search';
 import Card from '../Card/Card';
 import morty from '../../resources/img/morty.png';
+import rick from '../../resources/img/rick.png';
+import summer from '../../resources/img/summer.png';
 import Profile from '../Profile/profile';
 
 function App() {
+
   return (
     <Router>
-      <div className="App">
+      <div className="app">
       <HeaderMenu />
           <Switch>
             <Route exact path="/">
               <Banner />
               <Search />
-              <Card image={morty} cardName='Morty Smith' />
+              <div className='app__cards'>
+                <Card image={morty} cardName='Morty Smith' /> 
+                <Card image={rick} cardName='Rick Sanchez' /> 
+                <Card image={summer} cardName='Summer Smith' /> 
+              </div>
+            </Route>
+              <Route exact path="/location">
+            </Route>
+              <Route exact path="/episode">
+            </Route>
+            <Route exact path="/character">
               <Profile prof_img={morty} prof_name='Summer Smith' />
-            </Route>
-            <Route exact path="/location">
-            </Route>
-            <Route exact path="/episode">
             </Route>
           </Switch>
       </div>
--- src/components/InputText/inputText.js
+++ src/components/InputText/inputText.js
@@ -3,10 +3,11 @@ import { useState } from 'react';
 
 const InputText = () => {
 
-    const [value, setValue] = useState('Enter name');
+    const [value, setValue] = useState('');
 
     return(
-        <input className="inputText" type='text' value={value} onChange={event => setValue(event.target.value)}/>
+        <input className="inputText" type='text' value={value} placeholder='Enter name'
+        onChange={event => setValue(event.target.value)}/>
     )
 }
 
